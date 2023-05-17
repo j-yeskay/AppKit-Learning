61b2a2850f892cb8f77fbc4dd5a872963cd00daf
Sathish Kumar <asksathishk@gmail.com>
2023-05-17 18:28:23 +0530

    Double ScrollView

--- ScrollViewDemo/ScrollViewDemo/ViewController.swift
+++ ScrollViewDemo/ScrollViewDemo/ViewController.swift
@@ -5,6 +5,7 @@
 //  Created by sathish-pt6686 on 17/05/23.
 //
 
+
 import Foundation
 import AppKit
 
@@ -41,71 +42,53 @@ public class ViewController : NSViewController{
         
         self.view.addSubview(scrollView)
         
-        
         let redView = NSView()
         redView.translatesAutoresizingMaskIntoConstraints = false
         redView.wantsLayer = true
         redView.layer?.backgroundColor = NSColor.red.cgColor
-        
+
         let blueView = NSView()
         blueView.translatesAutoresizingMaskIntoConstraints = false
         blueView.wantsLayer = true
         blueView.layer?.backgroundColor = NSColor.blue.cgColor
-        
+
         let yellowView = NSView()
         yellowView.translatesAutoresizingMaskIntoConstraints = false
         yellowView.wantsLayer = true
         yellowView.layer?.backgroundColor = NSColor.yellow.cgColor
         
+        let greenView = NSView()
+        greenView.translatesAutoresizingMaskIntoConstraints = false
+        greenView.wantsLayer = true
+        greenView.layer?.backgroundColor = NSColor.green.cgColor
+        
+        let whiteView = NSView()
+        whiteView.translatesAutoresizingMaskIntoConstraints = false
+        whiteView.wantsLayer = true
+        whiteView.layer?.backgroundColor = NSColor.white.cgColor
+        
+        let purpleView = NSView()
+        purpleView.translatesAutoresizingMaskIntoConstraints = false
+        purpleView.wantsLayer = true
+        purpleView.layer?.backgroundColor = NSColor.purple.cgColor
+        
+        
+        
 
         docView.addSubview(redView)
         docView.addSubview(yellowView)
         docView.addSubview(blueView)
-
+        docView.addSubview(greenView)
+        docView.addSubview(purpleView)
+        docView.addSubview(whiteView)
         
-//        NSLayoutConstraint.activate([
-//            scrollView.leadingAnchor.constraint(equalTo: self.view.leadingAnchor),
-//            scrollView.trailingAnchor.constraint(equalTo: self.view.trailingAnchor),
-//            scrollView.topAnchor.constraint(equalTo: self.view.topAnchor),
-//            scrollView.bottomAnchor.constraint(equalTo: self.view.bottomAnchor),
-//
-//
-//            clipView.leadingAnchor.constraint(equalTo: scrollView.leadingAnchor),
-//            clipView.topAnchor.constraint(equalTo: scrollView.topAnchor),
-//            clipView.trailingAnchor.constraint(equalTo: scrollView.trailingAnchor),
-//            clipView.bottomAnchor.constraint(equalTo: scrollView.bottomAnchor),
-//
-//            docView.leadingAnchor.constraint(equalTo: clipView.leadingAnchor),
-//            docView.topAnchor.constraint(equalTo: clipView.topAnchor),
-//            docView.trailingAnchor.constraint(equalTo: clipView.trailingAnchor),
-//
-//            redView.leadingAnchor.constraint(equalTo: docView.leadingAnchor),
-//            redView.trailingAnchor.constraint(equalTo: docView.trailingAnchor),
-//            redView.topAnchor.constraint(equalTo: docView.topAnchor),
-//            redView.heightAnchor.constraint(equalTo: scrollView.heightAnchor),
-//
-//
-//            blueView.leadingAnchor.constraint(equalTo: docView.leadingAnchor),
-//            blueView.trailingAnchor.constraint(equalTo: docView.trailingAnchor),
-//            blueView.topAnchor.constraint(equalTo: redView.bottomAnchor),
-//            blueView.heightAnchor.constraint(equalTo: scrollView.heightAnchor),
-//
-//            yellowView.leadingAnchor.constraint(equalTo: docView.leadingAnchor),
-//            yellowView.trailingAnchor.constraint(equalTo: docView.trailingAnchor),
-//            yellowView.topAnchor.constraint(equalTo: blueView.bottomAnchor),
-//            yellowView.heightAnchor.constraint(equalTo: scrollView.heightAnchor),
-//
-//
-//            yellowView.bottomAnchor.constraint(equalTo: docView.bottomAnchor),
-//
-//
-//        ])
-//
+        
+        
         NSLayoutConstraint.activate([
-            scrollView.leadingAnchor.constraint(equalTo: self.view.leadingAnchor),
-            scrollView.trailingAnchor.constraint(equalTo: self.view.trailingAnchor),
-            scrollView.topAnchor.constraint(equalTo: self.view.topAnchor),
-            scrollView.bottomAnchor.constraint(equalTo: self.view.bottomAnchor),
+            scrollView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            scrollView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            scrollView.topAnchor.constraint(equalTo: view.topAnchor),
+            scrollView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
 
             clipView.leadingAnchor.constraint(equalTo: scrollView.leadingAnchor),
             clipView.topAnchor.constraint(equalTo: scrollView.topAnchor),
@@ -113,31 +96,51 @@ public class ViewController : NSViewController{
             clipView.bottomAnchor.constraint(equalTo: scrollView.bottomAnchor),
 
             docView.leadingAnchor.constraint(equalTo: clipView.leadingAnchor),
+
             docView.topAnchor.constraint(equalTo: clipView.topAnchor),
+
+//          FIRST COLUMN
 
             redView.leadingAnchor.constraint(equalTo: docView.leadingAnchor),
             redView.topAnchor.constraint(equalTo: docView.topAnchor),
-            redView.heightAnchor.constraint(equalTo: scrollView.heightAnchor),
-            redView.bottomAnchor.constraint(equalTo: docView.bottomAnchor),
-            redView.widthAnchor.constraint(equalTo: scrollView.widthAnchor),
+            redView.heightAnchor.constraint(equalTo: view.heightAnchor),
+            redView.widthAnchor.constraint(equalTo: view.widthAnchor),
 
+            blueView.leadingAnchor.constraint(equalTo: docView.leadingAnchor),
+            blueView.topAnchor.constraint(equalTo: redView.bottomAnchor),
+            blueView.heightAnchor.constraint(equalTo: view.heightAnchor),
+            blueView.widthAnchor.constraint(equalTo: view.widthAnchor),
 
-            blueView.leadingAnchor.constraint(equalTo: redView.trailingAnchor),
-            blueView.topAnchor.constraint(equalTo: docView.topAnchor),
-            blueView.heightAnchor.constraint(equalTo: scrollView.heightAnchor),
-            blueView.bottomAnchor.constraint(equalTo: docView.bottomAnchor),
-            blueView.widthAnchor.constraint(equalTo: scrollView.widthAnchor),
+            yellowView.leadingAnchor.constraint(equalTo: docView.leadingAnchor),
+            yellowView.topAnchor.constraint(equalTo: blueView.bottomAnchor),
+            yellowView.heightAnchor.constraint(equalTo: view.heightAnchor),
+            yellowView.widthAnchor.constraint(equalTo: view.widthAnchor),
 
-
-            yellowView.leadingAnchor.constraint(equalTo: blueView.trailingAnchor),
-            yellowView.topAnchor.constraint(equalTo: docView.topAnchor),
-            yellowView.heightAnchor.constraint(equalTo: scrollView.heightAnchor),
             yellowView.bottomAnchor.constraint(equalTo: docView.bottomAnchor),
-            yellowView.widthAnchor.constraint(equalTo: scrollView.widthAnchor),
 
-            yellowView.trailingAnchor.constraint(equalTo: docView.trailingAnchor)
+//          SECOND COLUMN
 
+            greenView.leadingAnchor.constraint(equalTo: redView.trailingAnchor),
+            greenView.topAnchor.constraint(equalTo: docView.topAnchor),
+            greenView.heightAnchor.constraint(equalTo: view.heightAnchor),
+            greenView.widthAnchor.constraint(equalTo: view.widthAnchor),
+            greenView.trailingAnchor.constraint(equalTo: docView.trailingAnchor),
+
+            purpleView.topAnchor.constraint(equalTo: greenView.bottomAnchor),
+            purpleView.leadingAnchor.constraint(equalTo: blueView.trailingAnchor),
+            purpleView.heightAnchor.constraint(equalTo: view.heightAnchor),
+            purpleView.widthAnchor.constraint(equalTo: view.widthAnchor),
+            purpleView.trailingAnchor.constraint(equalTo: docView.trailingAnchor),
+
+            whiteView.topAnchor.constraint(equalTo: purpleView.bottomAnchor),
+            whiteView.leadingAnchor.constraint(equalTo: yellowView.trailingAnchor),
+            whiteView.heightAnchor.constraint(equalTo: view.heightAnchor),
+            whiteView.widthAnchor.constraint(equalTo: view.widthAnchor),
+            whiteView.trailingAnchor.constraint(equalTo: docView.trailingAnchor),
+
+            whiteView.bottomAnchor.constraint(equalTo: docView.bottomAnchor),
 
         ])
+
     }
 }
